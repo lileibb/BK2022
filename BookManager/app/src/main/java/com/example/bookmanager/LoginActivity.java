56064174a1b3ab714eb0ca29b0aac181f87c4821
lileibb <ll2510170117@163.com>
2022-10-05 15:11:34 +0800

    [200~设置appiron标题字体样式(方正舒体)~

--- BookManager/app/src/main/java/com/example/bookmanager/LoginActivity.java
+++ BookManager/app/src/main/java/com/example/bookmanager/LoginActivity.java
@@ -3,12 +3,15 @@ package com.example.bookmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private TextView ironTitle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +21,15 @@ public class LoginActivity extends AppCompatActivity {
         int width11 = wm.getDefaultDisplay().getWidth();//getWidth()过期
         int height11 = wm.getDefaultDisplay().getHeight();//同上过期
         Log.d("ll", "onCreate: 宽"+width11+"   高"+height11);
+        initView();
     }
+
+    //初始化View
+    private void initView() {
+        ironTitle =findViewById(R.id.ironTitle);
+        //设置appiron标题字体样式(方正舒体)
+        ironTitle.setTypeface(Typeface.createFromAsset(getAssets(),"font/FZSTK.TTF"));
+    }
+
+
 }
