9d523c9af0b712dea36ee40e5e0cf761928e69e7
lileibb <ll2510170117@163.com>
2022-10-05 14:48:39 +0800

    CircleImageVi登录界面圆图

--- BookManager/app/src/main/java/com/example/bookmanager/LoginActivity.java
+++ BookManager/app/src/main/java/com/example/bookmanager/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.bookmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //this是context对象,获取宽度和高度，更好的修改布局
+        WindowManager wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        int width11 = wm.getDefaultDisplay().getWidth();//getWidth()过期
+        int height11 = wm.getDefaultDisplay().getHeight();//同上过期
+        Log.d("ll", "onCreate: 宽"+width11+"   高"+height11);
     }
 }
